829ce022e34ab4d3fe40892597f785436351ef7e
ZimaOo <olegzov@bigmir.net>
2020-08-05 02:31:35 +0300

    1

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY" {}
+
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "/home/zima/aws/MyEC2study1.pem"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
+
+#=====================================================================
