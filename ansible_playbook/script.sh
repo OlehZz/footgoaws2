3def62c2dcbd7a9466be2a8eae11b2d40f538043
ZimaOo <olegzov@bigmir.net>
2020-08-09 19:13:44 +0300

    ansible

--- ansible_playbook/script.sh
+++ ansible_playbook/script.sh
@@ -3,4 +3,4 @@ cp /home/ubuntu/footgo/src/main/resources/application.properties.example /home/u
 sudo export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
 sed -i 's/spring.datasource.username=root/spring.datasource.username=footgo/g' /home/ubuntu/footgo/src/main/resources/application.properties
 sed -i 's/spring.datasource.password=pwd/spring.datasource.password=footgodb/g' /home/ubuntu/footgo/src/main/resources/application.properties
-sed -i 's/spring.datasource.url=jdbc:mysql:\/\/localhost/spring.datasource.url=jdbc:mysql:\/\/terraform-20200805143905691500000001.cnowmm0u4gtt.us-east-1.rds.amazonaws.com/g' /home/ubuntu/footgo/src/main/resources/application.properties
+sed -i 's/spring.datasource.url=jdbc:mysql:\/\/localhost/spring.datasource.url=jdbc:mysql:\/\/terraform-20200807210322959000000001.cnowmm0u4gtt.us-east-1.rds.amazonaws.com/g' /home/ubuntu/footgo/src/main/resources/application.properties
