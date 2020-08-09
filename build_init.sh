0b71c86bd83905502a22b02d6463a6726c05b111
ZimaOo <olegzov@bigmir.net>
2020-08-09 22:55:18 +0300

    build ami script

---
+++ build_init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ARTIFACT=`packer build -machine-readable ./ami/footgo_v2.json |awk -F, '$0 ~/artifact,0,id/ {print $6}'`
+AMI_ID=`echo $ARTIFACT | cut -d ':' -f2`
+echo 'variable "AMI_ID" { default = "'${AMI_ID}'" }' > amivar.tf
+aws s3 cp amivar.tf s3://footgo-project-devops/ami/amivar.tf
